dbe678a9a79b7e709c0e1e9e968117228d09bbad
ImStaked <82274336+ImStaked@users.noreply.github.com>
2021-11-29 23:35:31 -0600

    Update setup_casper_node.sh

--- setup_casper_node.sh
+++ setup_casper_node.sh
@@ -1,16 +1,20 @@
-echo "Please enter the sudo password"
-
-sudo echo "It wont work without sudo"
-
-sleep 3
+echo "Please set up your keys first"
+sudo -u casper echo "SECRET_KEY_HERE"  > /etc/casper/validator_keys/secret_key.pem
+sudo -u casper echo "PUB_HEX" > /etc/casper/validator_keys/public_key_hex
+sudo -u casper echo "PUB_KEY"  > /etc/casper/validator_keys/public_key.pem
 
 CASPER_VERSION=1_0_0
 CASPER_NETWORK=casper
-sudo apt-get update
-sudo apt install -y dnsutils software-properties-common git jq
+
+echo "Setting up Casper Node"
+echo "  *-Version - 1.0.0   *"
+echo "  *-Network - casper  *"
+sleep 3
+
+sudo apt-get update -qq
+sudo apt install dnsutils software-properties-common git jq libssl-dev pkg-config build-essential -qq
 sudo systemctl stop casper-node-launcher.service
-sudo apt remove -y casper-client
-sudo apt remove -y casper-node-launcher
+sudo apt remove casper-client casper-node-launcher -qq
 sudo rm /etc/casper/casper-node-launcher-state.toml
 sudo rm -rf /etc/casper/1_0_*
 sudo rm -rf /var/lib/casper/*
@@ -20,17 +24,13 @@ sudo apt-key add casper-repo-pubkey.asc
 sudo apt update
 sudo apt install casper-node-launcher casper-client
 cd ~
-sudo -u casper apt purge --auto-remove cmake
+sudo apt purge --auto-remove cmake
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 sudo apt update
 sudo apt install cmake -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-sudo apt install libssl-dev -y
-sudo apt install pkg-config -y
-sudo apt install build-essential -y
 
 BRANCH="1.0.20" \
     && git clone --branch ${BRANCH} https://github.com/WebAssembly/wabt.git "wabt-${BRANCH}" \
@@ -48,18 +48,6 @@ cd casper-node/
 git checkout release-1.4.1
 make setup-rs
 make build-client-contracts -j
-echo "enter the pubhex"
-read PUBHEX
-echo "enter the pubkey"
-read PUBKEY
-echo "enter the secret"
-read SCRT
-sudo -u casper echo $SCRT  > /etc/casper/validator_keys/secret_key.pem
-SCRT=1
-sudo -u casper echo $PUBHEX > /etc/casper/validator_keys/public_key_hex
-sudo -u casper echo $PUBKEY  > /etc/casper/validator_keys/public_key.pem
-PUBHEX=1
-PUBKEY=1
 sudo -u casper /etc/casper/pull_casper_node_version.sh $CASPER_NETWORK.conf $CASPER_VERSION
 sudo -u casper /etc/casper/config_from_example.sh $CASPER_VERSION
 KNOWN_ADDRESSES=$(sudo -u casper cat /etc/casper/$CASPER_VERSION/config.toml | grep known_addresses)
@@ -67,8 +55,10 @@ KNOWN_VALIDATOR_IPS=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' 
 IFS=' ' read -r KNOWN_VALIDATOR_IP _REST <<< "$KNOWN_VALIDATOR_IPS"
 echo $KNOWN_VALIDATOR_IP
 # Get trusted_hash into config.toml
-TRUSTED_HASH=$(casper-client get-block --node-address http://$KNOWN_VALIDATOR_IP:7777 -b 20 | jq -r .result.block.hash | tr -d '\n')
-if [ "$TRUSTED_HASH" != "null" ]; then sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/$CASPER_VERSION/config.toml; fi
+#TRUSTED_HASH=$(casper-client get-block --node-address http://$KNOWN_VALIDATOR_IP:7777 -b 20 | jq -r .result.block.hash | tr -d '\n')
+TRUSTED_HASH=6214f009fabbbd601307ab94229d1cf53fcb988f59884e68ab22645ad867dc69
+# if [ "$TRUSTED_HASH" != "null" ]; then sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/$CASPER_VERSION/config.toml; fi
+sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/$CASPER_VERSION/config.toml
 sudo -u casper curl -sSf genesis.casperlabs.io/casper/1_1_0/stage_1_1_0_upgrade.sh | sudo bash
 sudo -u casper curl -sSf genesis.casperlabs.io/casper/1_1_2/stage_upgrade.sh | sudo bash -
 sudo -u casper curl -sSf genesis.casperlabs.io/casper/1_2_0/stage_upgrade.sh | sudo bash -
