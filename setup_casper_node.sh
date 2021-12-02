92f9a80b225ffd4e2edc2304735d3645f9f8ce87
ImStaked <82274336+ImStaked@users.noreply.github.com>
2021-12-01 20:14:43 -0600

    Update setup_casper_node.sh

--- setup_casper_node.sh
+++ setup_casper_node.sh
@@ -1,22 +1,12 @@
-# Set up keys
-echo "Please set up your keys first"
-sudo -u casper echo "SECRET_KEY_HERE"  > /etc/casper/validator_keys/secret_key.pem
-sudo -u casper echo "PUB_HEX" > /etc/casper/validator_keys/public_key_hex
-sudo -u casper echo "PUB_KEY"  > /etc/casper/validator_keys/public_key.pem
+#!/bin/bash
 
-# Versions
 CASPER_VERSION=1_0_0
 CASPER_NETWORK=casper
 
-
-echo "Setting up Casper Node"
-echo "  *-Version - 1.0.0   *"
-echo "  *-Network - casper  *"
-sleep 3
-
-
 sudo apt-get update -qq
 sudo apt install dnsutils software-properties-common git jq libssl-dev pkg-config build-essential -yq
+sudo snap install rustup --classic
+
 sudo systemctl stop casper-node-launcher.service
 sudo apt remove casper-client casper-node-launcher -yq
 sudo rm /etc/casper/casper-node-launcher-state.toml
@@ -25,14 +15,12 @@ sudo rm -rf /var/lib/casper/*
 sudo apt purge --auto-remove cmake -yq
 echo "deb https://repo.casperlabs.io/releases" bionic main | sudo tee /etc/apt/sources.list.d/casper.list
 curl -O https://repo.casperlabs.io/casper-repo-pubkey.asc
+sudo apt-key add casper-repo-pubkey.asc 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' && sudo apt-key add casper-repo-pubkey.asc
 sudo apt update && sudo apt install casper-node-launcher casper-client cmake -yq
 cd ~
 
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
 BRANCH="1.0.20" \
     && git clone --branch ${BRANCH} https://github.com/WebAssembly/wabt.git "wabt-${BRANCH}" \
     && cd "wabt-${BRANCH}" \
@@ -49,8 +37,13 @@ cd casper-node/
 git checkout release-1.4.1
 make setup-rs
 make build-client-contracts -j
+
+
+
+echo "end of autosetup manually finish config"
+sleep 6000
+
 sudo -u casper /etc/casper/pull_casper_node_version.sh $CASPER_NETWORK.conf $CASPER_VERSION
-sudo -u casper /etc/casper/config_from_example.sh $CASPER_VERSION
 KNOWN_ADDRESSES=$(sudo -u casper cat /etc/casper/$CASPER_VERSION/config.toml | grep known_addresses)
 KNOWN_VALIDATOR_IPS=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$KNOWN_ADDRESSES")
 IFS=' ' read -r KNOWN_VALIDATOR_IP _REST <<< "$KNOWN_VALIDATOR_IPS"
@@ -62,6 +55,19 @@ echo $KNOWN_VALIDATOR_IP
 # https://cspr.live/block/  <---- Sometimes the known validator does not respond its not your node....
 
 TRUSTED_HASH=6214f009fabbbd601307ab94229d1cf53fcb988f59884e68ab22645ad867dc69
+
+echo "  **** Stop. Manually Configure from here please hit cntl+c then follow the instructions below  ****"
+# Set up keys 
+sudo -u casper nano  > /etc/casper/validator_keys/secret_key.pem
+sudo -u casper nano > /etc/casper/validator_keys/public_key_hex
+sudo -u casper nano  > /etc/casper/validator_keys/public_key.pem
+
+
+sudo -u casper /etc/casper/pull_casper_node_version.sh $CASPER_NETWORK.conf $CASPER_VERSION
+# Enter Custom Settings Now
+nano /etc/casper/1_0_0/config-example.toml
+sudo -u casper /etc/casper/config_from_example.sh $CASPER_VERSION
+
 sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/$CASPER_VERSION/config.toml
 sudo -u casper curl -sSf genesis.casperlabs.io/casper/1_1_0/stage_1_1_0_upgrade.sh | sudo bash
 sudo -u casper curl -sSf genesis.casperlabs.io/casper/1_1_2/stage_upgrade.sh | sudo bash -
